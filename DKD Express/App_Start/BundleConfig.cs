0c025d5eaa4c3c63eed10833397db0ef0ce5b00f
Keanan Mohundass <keananm@astel.co.za>
2021-07-08 20:42:22 +0200

    email logic and css style fixes

--- DKD Express/App_Start/BundleConfig.cs
+++ DKD Express/App_Start/BundleConfig.cs
@@ -9,6 +9,7 @@ namespace DKD_Express
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                  "~/Scripts/global.js",
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -16,7 +17,7 @@ namespace DKD_Express
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(                      
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
--- DKD Express/Controllers/HomeController.cs
+++ DKD Express/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
-
+using DKD_Express.Resources;
 namespace DKD_Express.Controllers
 {
     public class HomeController : Controller
@@ -11,6 +13,37 @@ namespace DKD_Express.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(string email, string subject, string message, string name)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    MailMessage mm = new MailMessage("DevWorx3@gmail.com", email);
+                    mm.Subject = subject;
+                    mm.Body = message;
+                    mm.IsBodyHtml = false;
+
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new System.Net.NetworkCredential("DevWorx3@gmail.com", "Lee@940724");
+                    smtp.Send(mm);
+                }
+
+                return Json(new { message = labels.EmailSent, valid = true }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            
         }
 
         public ActionResult About()
